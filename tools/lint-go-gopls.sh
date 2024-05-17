7a3a59c02f493bc5d6b5f713a2e36bd5fed56ebe
priyagupta108 <priyagupta108@github.com>
2024-05-17 17:39:16 +0530

    test

---
+++ tools/lint-go-gopls.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+cd "$(dirname -- "${BASH_SOURCE[0]}")" && cd ..
+
+IGNORE_PATTERNS=(
+  "is deprecated"
+)
+
+# lint all go files with 'gopls check' and look for lines starting with the
+# current absolute path, indicating a error was found. This is neccessary
+# because the tool does not set non-zero exit code when errors are found.
+# ref: https://github.com/golang/go/issues/67078
+ERROR_LINES=$("$GO" run "$GOPLS_PACKAGE" check $@ 2>/dev/null | grep -E "^$PWD" | grep -vFf <(printf '%s\n' "${IGNORE_PATTERNS[@]}"));
+NUM_ERRORS=$(echo "$ERROR_LINES" | wc -l)
+
+if [ "$NUM_ERRORS" -eq "0" ]; then
+  exit 0;
+else
+  echo "$ERROR_LINES"
+  echo "Found $NUM_ERRORS 'gopls check' errors"
+  exit 1;
+fi
